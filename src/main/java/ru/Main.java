b3cf39fe01ee679856b18adc96bc7a6081b22003
tooryboard <annakonnik7@gmail.com>
2024-10-20 19:48:20 +0300

    1.3 Арифметические операции

--- src/main/java/ru/Main.java
+++ src/main/java/ru/Main.java
@@ -1,7 +1,18 @@
-package ru;
+package ru.calculator;
 
-public class Main {
+public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int one = 1;
+        int two = 2;
+        int onePlusTwo = one + two;
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int foutTimeTwo = four * two;
+
+        System.out.println("Деление: "sixDivTwo, "Вычитание: "fiveMinusTwo,"Умножение: "foutTimeTwo);
     }
 }
+
